eb1baf08ff74d8be069a30c5b07e86c3917b48d4
SupergrammerKim <supergrammer@naver.com>
2021-01-20 14:17:35 +0900

    [Feat] Add Register, Register Modal #3

--- Week_1/guestbook/src/main/java/Basecamp/guestbook/controller/GuestbookController.java
+++ Week_1/guestbook/src/main/java/Basecamp/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package Basecamp.guestbook.controller;
 
+import Basecamp.guestbook.dto.GuestbookDTO;
 import Basecamp.guestbook.dto.PageRequestDTO;
 import Basecamp.guestbook.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -27,4 +30,23 @@ public class GuestbookController {
 
         model.addAttribute("result", service.getList(pageRequestDTO));
     }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get.....");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto....." + dto);
+
+        Long gno = service.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
+    }
 }
+
+
+
