0662ea5941b6c66db83c7034b14aa30dc608c579
SupergrammerKim <supergrammer@naver.com>
2021-01-20 13:41:08 +0900

    [Feat] Add Page Link Process #3

--- Week_1/guestbook/src/main/java/Basecamp/guestbook/controller/GuestbookController.java
+++ Week_1/guestbook/src/main/java/Basecamp/guestbook/controller/GuestbookController.java
@@ -1,17 +1,30 @@
 package Basecamp.guestbook.controller;
 
+import Basecamp.guestbook.dto.PageRequestDTO;
+import Basecamp.guestbook.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookController {
-    @GetMapping({"/", "/list"})
-    public String list() {
-        log.info("list.............");
-        return "/guestbook/list";
+    public final GuestbookService service;
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+        log.info("list............." + pageRequestDTO);
+
+        model.addAttribute("result", service.getList(pageRequestDTO));
     }
 }
