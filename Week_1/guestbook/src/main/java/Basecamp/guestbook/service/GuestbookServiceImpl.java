f2708ad0b608fcc7fba0af744f01b1ba5552ec80
SupergrammerKim <supergrammer@naver.com>
2021-01-20 18:19:15 +0900

    [Feat] Search Module, Modify Redirectattributes #3

--- Week_1/guestbook/src/main/java/Basecamp/guestbook/service/GuestbookServiceImpl.java
+++ Week_1/guestbook/src/main/java/Basecamp/guestbook/service/GuestbookServiceImpl.java
@@ -4,7 +4,10 @@ import Basecamp.guestbook.dto.GuestbookDTO;
 import Basecamp.guestbook.dto.PageRequestDTO;
 import Basecamp.guestbook.dto.PageResultDTO;
 import Basecamp.guestbook.entity.Guestbook;
+import Basecamp.guestbook.entity.QGuestbook;
 import Basecamp.guestbook.repository.GuestbookRepository;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
@@ -39,7 +42,9 @@ public class GuestbookServiceImpl implements GuestbookService {
     public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
         Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
 
-        Page<Guestbook> result = repository.findAll(pageable);
+        BooleanBuilder booleanBuilder = getSearch(requestDTO);
+
+        Page<Guestbook> result = repository.findAll(booleanBuilder, pageable);
 
         Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
 
@@ -70,5 +75,39 @@ public class GuestbookServiceImpl implements GuestbookService {
 
             repository.save(entity);
         }
+    }
+
+    private BooleanBuilder getSearch(PageRequestDTO requestDTO) {
+        String type = requestDTO.getType();
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        String keyword = requestDTO.getKeyword();
+
+        BooleanExpression expression = qGuestbook.gno.gt(0L);
+
+        booleanBuilder.and(expression);
+
+        if (type == null || type.trim().length() == 0) {
+            return booleanBuilder;
+        }
+
+        BooleanBuilder conditionBuilder = new BooleanBuilder();
+
+        if (type.contains("t")) {
+            conditionBuilder.or(qGuestbook.title.contains(keyword));
+        }
+        if (type.contains("c")) {
+            conditionBuilder.or(qGuestbook.content.contains(keyword));
+        }
+        if (type.contains("w")) {
+            conditionBuilder.or(qGuestbook.writer.contains(keyword));
+        }
+
+        booleanBuilder.and(conditionBuilder);
+
+        return booleanBuilder;
     }
 }
