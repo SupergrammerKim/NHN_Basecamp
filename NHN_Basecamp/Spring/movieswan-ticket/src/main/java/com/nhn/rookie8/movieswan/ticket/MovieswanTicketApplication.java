ac627510cd8bbb6534b2e9b5ad8b7275191f53e0
Supergrammer <supergrammer@naver.com>
2021-02-03 14:35:21 +0900

    [Feat] Create Spring Directory

---
+++ NHN_Basecamp/Spring/movieswan-ticket/src/main/java/com/nhn/rookie8/movieswan/ticket/MovieswanTicketApplication.java
@@ -0,0 +1,13 @@
+package com.nhn.rookie8.movieswan.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieswanTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieswanTicketApplication.class, args);
+	}
+
+}
---
+++ NHN_Basecamp/Spring/movieswan-ticket/src/test/java/com/nhn/rookie8/movieswan/ticket/MovieswanTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhn.rookie8.movieswan.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieswanTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
